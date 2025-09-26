02595366e242cf49ad7691c3f07374d329b21b23
hawkarabdulhaq <111079979+hawkarabdulhaq@users.noreply.github.com>
2025-09-26 08:52:58 +0200

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
-assignment_bp = Blueprint ("assignment", __name__)
+assignment_bp = Blueprint("assignment", __name__)
+
 @assignment_bp.route("/assignment")
 def assignment_home():
-  return render)template("assignment.html")
+    return render_template("assignment.html")
